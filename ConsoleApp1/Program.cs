e4eafb8ac6efac06f5a95d4659c82beefba73f55
yourprettygood <79272017+yourprettygood@users.noreply.github.com>
2024-05-22 09:50:00 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача №1
+            /*
+            Console.Write("Введите x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write($"x + y = {x+y}");
+            Console.ReadLine();
+            */
+            //Задача №2
+            /*
+            Console.Write("Введите x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Введите z: ");
+            int z = int.Parse(Console.ReadLine());
+            Console.Write($"x + y + z = {x + z + y}");
+            */
+            //Задача №3
+            /*
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            double y = double.Parse(Console.ReadLine());
+            double z = x * y;
+            Console.Write($"{x} * {y} = {z:f1}");
+            */
+            //Задача №4
+            /*
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            double y = double.Parse(Console.ReadLine());
+            double z = x / y;
+            Console.Write($"{x:f3} / {y:f3} = {z:f3}");
+            */
+            //Задача №6
+            /*
+            Console.Write("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine($"a + b = {x} + {y} = {x + y}\ta - b = {x} - {y} = {x-y}\ta * b = {x} * {y} = {x * y }");
+            */
+            //Задача №7
+            /*
+            Console.Write("Как вас зовут ?");
+            string name = Console.ReadLine();
+            Console.Write("Сколько тебе лет ?");
+            int age = int.Parse(Console.ReadLine());
+            DateTime dateTime = new DateTime();
+            Console.Write($"{name}, ты родился в {2024-age} году");
+            */
+            //Задача №8
+            /*
+            Console.Write("Номинал купюры: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Кол-во купюр: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write($"Сумма денег: {x*y:f2}р.");
+            */
+            //Задача №9
+            /*
+            Console.Write("Введите сторону квадрта:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write($"Периметр = {a * 4}");
+            */
+            //Задача 10
+
+            Console.ReadLine();
+        }
+    }
+}
